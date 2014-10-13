0f4bd97a9e8e78e320d9ebcbd91ba1ef4c4be4ca
Pavel Kamyshov <pkamyshov@gmail.com>
2014-10-13 21:10:03 +0300

    Another portion of files

---
+++ src/main/java/YandexTest.java
@@ -0,0 +1,100 @@
+/**
+ * Created by Pavlo_Kamyshov on 10/13/2014.
+ */
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class YandexTest {
+
+    private static final String PASSWORD = "Test1234";
+  //  private static final String DOMAIN_PART = "@inbox.ru";
+    private static final String LOGIN = "pavkam2014";
+    private static final String START_URL = "https://mail.yandex.com/";
+
+    private static final String LETTER_SUBJECT = "Demo sending via WebDriver";
+    private static final String LETTER_BODY = "New email!";
+    private WebDriver driver;
+
+    @BeforeClass(description = "Start browser")
+    public void startBrowser() {
+        System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(START_URL);
+    }
+
+    @BeforeClass(dependsOnMethods = "startBrowser", description = "Add implicitly")
+    public void addImplicitly() {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test(description = "Login to Yandex.ru")
+    public void loginToYandex() {
+        doLogin(LOGIN, PASSWORD);
+        Assert.assertTrue(isElementPresent(By.id("PH_logoutLink")));
+    }
+
+    @Test(description = "Begin new letter creation", dependsOnMethods = { "loginToYandex" })
+    public void beginCreationOfLetter() {
+        driver.findElement(By.xpath("//a[@data-name='compose']")).click();
+        Assert.assertTrue(driver.getCurrentUrl().contains("compose"));
+    }
+
+    @Test(description = "Send new letter", dependsOnMethods = { "beginCreationOfLetter" })
+    public void sendNewLetter() {
+        sendLetter(LOGIN + "@yandex.ru", LETTER_SUBJECT, LETTER_BODY);
+        Assert.assertTrue(isElementPresent(By.xpath("//div[@class='message-sent__title']")));
+    }
+
+    @AfterClass(description = "Stop Browser")
+    public void stopBrowser() {
+        driver.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        return !driver.findElements(by).isEmpty();
+    }
+
+    private void doLogin(String login, String password) {
+
+        WebElement loginInput = driver.findElement(By.id("b-mail-domik-username11"));
+        loginInput.clear();
+        loginInput.sendKeys(login);
+
+//        Select domainPartSelect = new Select(driver.findElement(By.xpath("//*[@id='mailbox__login__domain']")));
+//        domainPartSelect.selectByVisibleText(domainPart);
+
+        WebElement passwordInput = driver.findElement(By.id("b-mail-domik-password11"));
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+
+        driver.findElement(By.xpath("//input[@value='Log in']")).click();//(By.tagNamid("mailbox__auth__button")).click();
+    }
+
+    private void sendLetter(String to, String subject, String body) {
+        driver.findElement(By.xpath("//input[@data-original-name='To']")).sendKeys(to);
+        driver.findElement(By.name("Subject")).sendKeys(subject);
+
+        // doesn't work ???
+        // driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='compose_330_composeEditor_ifr']")));
+
+        driver.switchTo().frame(3); // WTF Magic!?
+
+        WebElement bodyInput = driver.findElement(By.id("tinymce"));
+        bodyInput.clear();
+        bodyInput.sendKeys(body);
+
+        driver.switchTo().defaultContent();
+        driver.findElement(By.xpath("//div[@data-name='send']/span")).click();
+    }
+
+}
