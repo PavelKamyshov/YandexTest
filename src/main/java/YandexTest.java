e7531b66e013cd52c95d8e19d28191c2efcd10bd
PavelKamyshov <pkamyshov@gmail.com>
2014-10-17 00:44:19 +0300

    Works letter To, Subject, Description.

--- src/main/java/YandexTest.java
+++ src/main/java/YandexTest.java
@@ -4,6 +4,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -49,7 +50,7 @@ public class YandexTest {
         driver.findElement(By.xpath("//*[@id=\"js\"]/body/div[2]/div/div[5]/div/div[2]/div/div[2]/div/div/div/div[2]/a[2]/span[1]")).click();
         //<input type="submit" tabindex="4" class="b-mail-button__button" value="Log in">
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-      //  Assert.assertTrue(driver.getCurrentUrl().contains("compose"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("/#com"));
     }
 
     @Test(description = "Send new letter", dependsOnMethods = { "beginCreationOfLetter" })
@@ -86,22 +87,24 @@ public class YandexTest {
 
     private void sendLetter(String to, String subject, String body) {
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-           //driver.findElement(By.xpath("//input[@data-original-name='To']")).sendKeys(to);
-       driver.findElement(By.xpath("//div[contains(@class, 'mail-input_to')]/input[contains(@class, 'focus')]")).sendKeys(to);
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        driver.findElement(By.id("compose-subj")).sendKeys(subject);
+        WebElement toField = driver.findElement(By.xpath("//div[contains(@class, 'mail-input_to')]/input[contains(@class, 'focus')]"));
+//  System.out.println("before - " + toField.getAttribute("disabled"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].removeAttribute('disabled');", toField);
+//  System.out.println("after - " + toField.getAttribute("disabled"));
+        toField.sendKeys(to);
+         driver.findElement(By.id("compose-subj")).sendKeys(subject);
 
         // doesn't work ???
         // driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='compose_330_composeEditor_ifr']")));
-
-        driver.switchTo().frame(3); // WTF Magic!?
+      //!!! driver.switchTo().frame(driver.findElement(By.id("compose-send")));
+       // driver.switchTo().frame(3); // WTF Magic!?
 
         WebElement bodyInput = driver.findElement(By.id("compose-send"));
         bodyInput.clear();
         bodyInput.sendKeys(body);
 
-        driver.switchTo().defaultContent();
-        driver.findElement(By.xpath("//div[@data-name='send']/span")).click();
+      //  driver.switchTo().defaultContent();
+      //  driver.findElement(By.id("compose-submit")).click();
 
     }
 
